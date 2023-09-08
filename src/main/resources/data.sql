39c61097b110e05bb86e768ea733c276d29c3717
jacquelinemangus <132783246+jacquelinemangus@users.noreply.github.com>
2023-09-08 15:31:25 -0600

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,14 @@
+select * from cheese;
+select * from customer;
+select * from cheese_order;
+select * from macaroni_orders;
+select * from noodle_shape;
+
 INSERT INTO macaroni_orders.noodle_shape (macaroni, rotini, farfalle) VALUES('test', 'test', 'test');
+INSERT INTO macaroni_orders.cheese (cheese_type, cheese_region, cheese_spice_level) VALUES('cheddar', 'england', 'medium');
+INSERT INTO macaroni_orders.cheese (cheese_type, cheese_region, cheese_spice_level) VALUES('brie', 'france', 'mild');
+INSERT INTO macaroni_orders.cheese (cheese_type, cheese_region, cheese_spice_level) VALUES('american', 'american', 'mild');
+INSERT INTO macaroni_orders.cheese (cheese_type, cheese_region, cheese_spice_level) VALUES('colby jack', 'america', 'medium');
+INSERT INTO macaroni_orders.macaroni_orders (noodle_shape_id, customer_id, herb_topping_choice) VALUES('1', '1', 'oregano');
+INSERT INTO macaroni_orders.macaroni_orders (noodle_shape_id, customer_id, herb_topping_choice) VALUES('2', '2', 'basil');
+INSERT INTO macaroni_orders.cheese_order (macaroni_order_id , cheese_id) VALUES('2', '2');
